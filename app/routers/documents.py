c0c954c3817cfb310be36ad5c760e66a168621b1
Ibrahim <sehosseini@me.com>
2026-06-09 06:17:21 +0330

    Add document upload and retrieval endpoints in documents.py
    - Implemented a POST endpoint to upload PDF documents, validate file types, and store them in the uploads directory.
    - Added a GET endpoint to retrieve a list of uploaded documents.
    - Integrated document creation and indexing functionality with the database.
    - Ensured proper error handling and session management during document operations.

---
+++ app/routers/documents.py
@@ -0,0 +1,45 @@
+from fastapi import APIRouter, Depends, HTTPException, UploadFile, File, status
+from app.services.indexing_service import index_document
+from db.session import get_db
+from app.schemas.document import DocumentRequest, DocumentResponse
+from db.repository import document_repository
+import shutil, uuid
+from pathlib import Path
+
+router = APIRouter(
+    prefix="/documents",
+    tags=["documents"]
+)
+
+UPLOAD_DIR = Path("uploads")
+UPLOAD_DIR.mkdir(exist_ok=True)
+ALLOWED_EXTENSIONS = {".pdf"}
+
+@router.post("/",response_model=DocumentResponse, status_code=status.HTTP_201_CREATED)
+async def upload_document(file: UploadFile = File(...), session=Depends(get_db)):
+    
+    file_ext = Path(file.filename).suffix
+    unique_name = f"{uuid.uuid4()}{file_ext}"
+    file_path = UPLOAD_DIR/unique_name
+
+    if file_ext.lower() not in ALLOWED_EXTENSIONS:
+        raise HTTPException(status_code=400, detail="Only PDF files are allowed")
+    with file_path.open("wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    document_request = DocumentRequest(file_name=unique_name, file_path=str(file_path))
+
+    try:
+        new_document = await document_repository.create_document(session=session, document=document_request)
+        await index_document(path=str(file_path), document_id=new_document.id, session=session)
+        await session.commit()
+        return new_document
+    except Exception as e:
+        await session.rollback()
+        raise HTTPException(status_code=500, detail=str(e))
+    
+
+@router.get("/", response_model=list[DocumentResponse])
+async def get_documents(session=Depends(get_db)):
+    response = await document_repository.get_document(session=session)
+    return response
